ec99c3f37dba7453a40ff4c1416037bebf3a6398
david9833 <dagalisteo@gmail.com>
2022-12-30 18:18:35 +0000

    segundo avance login

--- src/js/views/login.js
+++ src/js/views/login.js
@@ -9,27 +9,38 @@ export const Login = () => {
         <div className="fluid-container">
 
             <div className="row bg-success justify-content-center">
-                <div className={`col bg-primary show-div`}  >
-                <img src={login} className="img-fluid " alt="..."/>
+                <div className="col show-div bg-primary"  >
+                <img src={login} className="img-fluid"  alt="..."/>
                 </div>
 
                 <div className="col bg-color">
                     <h1 className="text-center title">Login</h1>
                     <form>
                         
-                        <div>
-                            <h2>Email</h2>
-                            <label></label>
-                            <input  placeholder="Email"/>
+                        <div className="p-2">
+                            <h3>Email</h3>
+                            <label ></label>
+                            <input className="input" type="text"  placeholder="Email"/>
                         </div>
-                        <div>
-                            <h2>Password</h2>
+                        <div className="p-2">
+                            <h3>Password</h3>
                             <label></label>
-                            <input placeholder="Password"/>
+                            <input className="input" placeholder="Password"/>
                         </div>
-
-                        <input type="submit"/>
+                        
+                        <div className="text-end" style={{width:"510px"}}>
+                            <input type="submit" className="button"/>
+                        </div>
+                        
                     </form>
+
+                    <div className="text-end mt-4" style={{width:"510px"}}>
+                            <p>Forgot your Password?</p>
+                    </div>
+
+                    <div className="text-center mt-5">
+                        <h3 className="register">Not user yet? Sign Up</h3>
+                    </div>
                 </div>
             </div>
 
